813234f117111186d82816bc5f0105daad328242
Shubham Patrick <ahthneeuhl@gmail.com>
2023-04-16 12:06:42 +0530

    Create Dashboard Layout and Active Friend Requests

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
 export { default as AddFriendButton } from "./AddFriendButton";
 export { default as Providers } from "./Providers";
+export { default as SignOutButton } from "./SignOutButton";
+export { default as SidebarChatList } from "./SidebarChatList";
+export { default as FriendRequestSidebarOptions } from "./FriendRequestSidebarOptions";
+export { default as FriendRequests } from "./FriendRequests";
